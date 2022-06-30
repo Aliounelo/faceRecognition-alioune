7109e8669ce5c3926a9ed1ec088c684a5bce5fe7
Alioune LO <alioune_2002@hotmail.fr>
2022-06-30 10:11:02 +0000

    tests on different images

--- Face Detection/FaceDetection-Haar.py
+++ Face Detection/FaceDetection-Haar.py
@@ -23,7 +23,7 @@ def detectFaces(cascade, img_copy, scaleFactor=1.027) :
         cv2.rectangle(img_copy, (x,y), (x+w, y+h), (255, 0, 0), 2)
 
     #-- Display Faces
-    cv2.imshow('Capture - Face Detection', img_copy)    
+    #cv2.imshow('Capture - Face Detection', img_copy)    
     print("Faces found :", len(faces))
 
 haar_cascade =  cv2.CascadeClassifier('../opencv-3.4/data/haarcascades/haarcascade_frontalface_alt.xml')
@@ -37,23 +37,14 @@ ta = time.time()
 lbp = detectFaces(lbp_cascade,img)
 tb = time.time()
 dt = tb- ta
+print(dt)
 
 #-- Time execution for Haar Cascade 
 t1 = time.time()
 haar = detectFaces(haar_cascade,img)
 t2 = time.time()
 dt1 = t2 - t1
-
-#-- Let's do the comparison
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-
-ax1.set_title('Haar Detection time: ' + str(round(dt1, 3)) + ' secs')
-ax1.imshow(haar)
-
-ax2.set_title('LBP Detection time: ' + str(round(dt, 3)) + ' secs')
-ax2.imshow(lbp)
-
-
+print(dt1)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
