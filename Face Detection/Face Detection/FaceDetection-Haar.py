b0d6831f0394c15bcb6a443debec38d11a602b69
Alioune LO <alioune_2002@hotmail.fr>
2022-06-30 09:20:03 +0000

    ...

--- Face Detection/Face Detection/FaceDetection-Haar.py
+++ Face Detection/Face Detection/FaceDetection-Haar.py
@@ -2,7 +2,7 @@
 Haar Cascade Face detection with OpenCV  
     Based on tutorial by superdatascience.com && docs.opencv.org
     Visit original posts: https://www.superdatascience.com/blogs/opencv-face-recognition, https://docs.opencv.org/3.4/db/d28/tutorial_cascade_classifier.html
-Adapted by Alioune LO - Z01D3R @ 22Jun2022 
+Adapted by Alioune LO - Z01D3R @ 7Jun2022 
 
 """
 
@@ -23,19 +23,19 @@ def detectFaces(cascade, img_copy, scaleFactor=1.027) :
         cv2.rectangle(img_copy, (x,y), (x+w, y+h), (255, 0, 0), 2)
 
     #-- Display Faces
-    #cv2.imshow('Capture - Face Detection', img_copy)    
+    cv2.imshow('Capture - Face Detection', img_copy)    
     print("Faces found :", len(faces))
 
-haar_cascade =  cv2.CascadeClassifier('../../opencv-3.4/data/haarcascades/haarcascade_frontalface_alt.xml')
-lbp_cascade = cv2.CascadeClassifier('../../opencv-3.4/data/lbpcascades/lbpcascade_frontalface.xml')
-img = cv2.imread('../../images/multiFace.jpeg')
+haar_cascade =  cv2.CascadeClassifier('../opencv-3.4/data/haarcascades/haarcascade_frontalface_alt.xml')
+lbp_cascade = cv2.CascadeClassifier('../opencv-3.4/data/lbpcascades/lbpcascade_frontalface.xml')
+img = cv2.imread('../images/multiFace.jpeg')
 
 #-- Time execution for LBP cascade
 ta = time.time()
 detectFaces(lbp_cascade,img)
 tb = time.time()
 dt = tb- ta
-print('lbp :',dt)
+print(dt)
 
 
 #-- Time execution for Haar Cascade 
@@ -43,7 +43,7 @@ t1 = time.time()
 detectFaces(haar_cascade,img)
 t2 = time.time()
 dt1 = t2 - t1
-print('haar :',dt1) 
+print(dt1) 
 
 
 cv2.waitKey(0)
